c1fd9791d298b5ac62916b986ad8becf15bcfd2a
Nathan Thomas <nwthomas@me.com>
2019-03-01 08:22:11 -0800

    Create generateToken function

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,30 @@
-const axios = require('axios');
+const axios = require("axios");
+require("dotenv").config();
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate } = require("../auth/authenticate");
+const db = require("../database/dbConfig.js");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+function generateToken(user) {
+  const payload = {
+    username: user.id,
+    name: user.username
+  };
+
+  const secret = process.env.JWT_SECRET;
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
@@ -18,15 +37,15 @@ function login(req, res) {
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
